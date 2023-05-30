965d613f72328ca9b715c24b5a0283846a7cda66
son.tran <son.tran@team.enouvo.com>
2023-05-30 11:56:15 +0700

    feat(master): fix tsconfig

--- src/shared/middlewares/logger.middleware.ts
+++ src/shared/middlewares/logger.middleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction } from 'express';
+import { Request, Response, NextFunction } from 'express';
 
 export function logger(req: Request, res: Response, next: NextFunction) {
   next();
