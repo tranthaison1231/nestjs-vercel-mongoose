deb7082347a58ccdad34054fffe8febae4ae596f
son.tran <son.tran@team.enouvo.com>
2023-06-10 14:42:19 +0700

    feat(master): make JWT secret is env

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,21 +2,26 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 import { PassportModule } from '@nestjs/passport';
-import { EXPIRES_IN, JWT_SECRET } from '../../shared/constants /config';
+import { EXPIRES_IN } from '../../shared/constants /config';
 import { User, UserSchema } from '../users/schemas/user.shema';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { JwtStrategy } from './jwt.strategy';
-import { ConfigService } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: JWT_SECRET,
-      signOptions: {
-        expiresIn: EXPIRES_IN,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService) => {
+        return {
+          secret: configService.get('JWT_SECRET'),
+          signOptions: {
+            expiresIn: EXPIRES_IN,
+          },
+        };
       },
     }),
     UsersModule,
--- src/shared/constants /config.ts
+++ src/shared/constants /config.ts
@@ -1,2 +1,1 @@
 export const EXPIRES_IN = 3600;
-export const JWT_SECRET = 'secretKey';
